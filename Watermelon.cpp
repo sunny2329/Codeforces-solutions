730896493024a1630c5ba029f18411b135e82c50
Sunny <sobhit230@gmail.com>
2023-01-26 09:40:53 +0530

    change

---
+++ Watermelon.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+/*
+ By Sobhit Sinha
+
+ */
+
+#include <algorithm>
+#include <math.h>
+#include <cctype>
+#include <cmath>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+
+// reverse a string
+void Reverse(string& sl){
+    reverse(sl.begin(),sl.end());
+}
+
+// sum of an array
+int suma(int ar[], int size){
+    int sum=0;
+    for (int i = 0; i < size; ++i)
+    {
+        sum += ar[i];
+    }
+    return sum;
+}
+
+// factorial
+int factorial(int n){
+    int fact=1;
+    for (int i = 1; i <=n; ++i)
+    {
+        fact = fact*i;
+    }
+    return fact;
+}
+
+// to upper case
+void upper(string& s){
+    transform(s.begin(),s.end(),s.begin(), ::toupper);
+}
+
+// to lower case
+void lower(string& s){
+    transform(s.begin(),s.end(),s.begin(), ::tolower);
+}
+ 
+// checking prime number
+bool isPrime(int n)
+{
+    // Corner case
+    if (n <= 1)
+        return false;
+ 
+    // Check from 2 to n-1
+    for (int i = 2; i < n; i++)
+        if (n % i == 0)
+            return false;
+ 
+    return true;
+}
+
+int main(){
+#ifndef ONLINE_JUDGE
+freopen("input4.txt","r",stdin);
+freopen("output4.txt","w",stdout);
+#endif 
+int w;
+cin >> w;
+if((w%2==0) && (w!=2)) cout << "YES";
+else cout << "NO";
+}
